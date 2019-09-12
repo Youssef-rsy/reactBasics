da2e0167eb4fb151cb07af4cbdb7000df25d5cd4
Youssef-rsy <youssef.rsy@gmail.com>
2019-09-12 17:19:28 +0100

    react basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state ={
+    person : [
+      {id:1, name:'Youssef' , age:24},
+      {id:2, name:'Max' , age:28},
+      {id:3, name:'Stephanie' , age:26},
+    ],
+    showPerson:false
+  }
+
+  showPersones = ()=>{
+    this.setState({
+      showPerson : !this.state.showPerson
+    })
+  }
+  setNewName= (event,id)=> {
+    const persons = [...this.state.person];
+    const personIndex =persons.findIndex(p=> {
+      return p.id===id
+    })
+    persons[personIndex].name = event.target.value;
+    this.setState({
+      person : persons,
+    })
+  }
+  deletePerson = (personIndex)=>{ 
+    console.log("let's delet this shit indexed by "+personIndex);
+    //const persons = this.state.person.slice(); 
+    //equivalant in ES6 
+    const persons = [...this.state.person]; // the spreads operation
+    persons.splice(personIndex,1);
+    this.setState({
+      person:persons
+    })
+  }
+
+  render(){
+    const style= {
+      backgroundColor:'blue',
+      font:'inherit',
+      border:'1px solid blue',
+      padding :'8px',
+      cursor:'pointer'
+    }
+
+    let persons = null;
+    if( this.state.showPerson ){
+      persons = ( <div>
+        { 
+          this.state.person.map((person, index)=>{
+          return <Person 
+          key={person.id} 
+          click={() => this.deletePerson(index)}
+          change={(event)=>this.setNewName(event ,person.id)}
+          name={person.name} 
+          age={person.age}/>
+        })}
+      </div>);
+      style.backgroundColor = 'red';
+    }
+    
+    let classes = ['red','bold'].join(' ');//red bold
+      
+
+    return (
+      <div className="App">
+        <h1> Hi i'm REact App</h1>
+        <p className={classes}>This.is really working</p>
+        <button 
+          style={style}
+          onClick={this.showPersones}> Switch</button>
+          {persons}
+          
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/AppUseState.js
@@ -0,0 +1,47 @@
+import React , {Component ,useState} from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const AppUseState = (props) =>  {
+
+  const [personState , setPersonSate] = useState({
+    person : [
+      {name:'Youssef' , age:24},
+      {name:'Max' , age:28},
+      {name:'Stephanie' , age:26},
+    ]
+  });
+
+
+  const [dummyData , setDumyData] = useState({
+    title:'React Tuto' 
+  });
+
+
+  const swithchAge = ()=>{
+    setDumyData({
+        title:'React Tuto !!!!!' 
+      });
+    setPersonSate({
+      person : [
+        {name:'Youssef Rossamy ' , age:24},
+        {name:'Maxumiaum nava' , age:28},
+        {name:'Stephanie okh' , age:26},
+      ]
+    });
+    
+  }
+  
+    return (
+      <div className="App">
+        <h1> Welcom to the "{dummyData.title}"</h1>
+        <button onClick={swithchAge}> Switch</button>
+        <Person name={personState.person[0].name} age={personState.person[0].age}/>
+        <Person name={personState.person[1].name} age={personState.person[1].age}>and I love : Code</Person>
+        <Person name={personState.person[2].name} age={personState.person[2].age}/>
+      </div>
+    );
+  }
+
+
+export default AppUseState;
